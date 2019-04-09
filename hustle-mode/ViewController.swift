5ce65bb5f3a2d75e311e8c7e11502cc8103f0655
Ahmed Adnan <ios@rosenfielddwc.com>
2019-04-09 13:50:58 +0200

    first try to build ios application and make animation

--- hustle-mode/ViewController.swift
+++ hustle-mode/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
 
+    @IBOutlet weak var DarkblueBG: UIImageView!
+    @IBOutlet weak var CloudHolder: UIView!
+    @IBOutlet weak var onlb: UILabel!
+    @IBOutlet weak var hustlelbl: UILabel!
+    @IBOutlet weak var rocet: UIImageView!
+    @IBOutlet weak var PowerBtn: UIButton!
+    var player : AVAudioPlayer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path=Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do{
+            player=try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        }catch let error as NSError{
+            print(error.description)
+        }
+        
+        
     }
 
-
+    @IBAction func Powebuttonpressd(_ sender: Any) {
+        CloudHolder.isHidden=false
+        DarkblueBG.isHidden=true
+        PowerBtn.isHidden=true
+        player.play()
+        UIView.animate(withDuration: 2.3, animations:{
+            self.rocet.frame=CGRect(x: 0, y: 140, width: 375, height: 402)
+        }){(finished) in self.hustlelbl.isHidden=false
+            self.onlb.isHidden=false
+        }
+    }
+    
 }
 
